0e7b7e524e24732063a6f6a90a4500e7e117fec0
daniel_robb@taylor.edu <drobb@cse21714.cs.local>
2020-10-05 14:07:50 -0400

    updated db.js and added test/

---
+++ api/db.js
@@ -0,0 +1,13 @@
+const knex = require('knex') ({
+	client: 'pg',
+	connection: {
+		host: 'faraday.cse.taylor.edu',
+		user: 'daniel_robb',
+		password: 'zohopefu',
+		database: 'daniel_robb'
+	}
+});
+
+objection = require('objection');
+const Model = objection.Model;
+Model.knex(knex);
