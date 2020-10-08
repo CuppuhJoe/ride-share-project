069a35d78bd21f076638254a38f151f37ba0cc70
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-10-08 09:32:55 -0400

    Update to use dotenv; invoke `insert` with `then`

--- api/db.js
+++ api/db.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
+
 // Connect knex to the database.
-const knex = require('knex') ({
-	client: 'pg',
-	connection: {
-		host: 'faraday.cse.taylor.edu',
-		user: 'daniel_robb',
-		password: 'zohopefu',
-		database: 'daniel_robb'
-	}
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  },
 });
 
 // Connect objection to knex.
--- api/test/test-user.js
+++ api/test/test-user.js
@@ -1,41 +1,38 @@
 const User = require("../models/User.js");
 
-
 const crickson = User.query().insert({
-  firstName: 'Crickson',
-  lastName: 'Bain',
-  email: 'email@email.com',
-  password: '2cool4u',
+  firstName: "Crickson",
+  lastName: "Bain",
+  email: "email@email.com",
+  password: "2cool4u",
   phone: 1234560,
-  isAdmin: false
-});
+  isAdmin: false,
+}).then(result => console.log(result));
 
 // User.query()
 // 	.insert(crickson)
 // 	.catch((err) => console.log(err.message));
 
-
-const {raw} = require("objection");
+const { raw } = require("objection");
 
 // const numDeleted = User.query()
 // 	.deleteById(4)
 //     .catch((err) => console.log(err.message));
 
 User.query()
-  .patch({ password: 'thisisyournewpasswordnow' })
+  .patch({ password: "thisisyournewpasswordnow" })
   .catch((err) => console.log(err.message));
 
-
 async function runTests() {
-	await User.query()
-	  .then((users) => {
-	    users.forEach((user) => {
-	      console.log("FIRSTNAME", user.firstName);
+  await User.query()
+    .then((users) => {
+      users.forEach((user) => {
+        console.log("FIRSTNAME", user.firstName);
 
-	      console.log(user);
-	    });
-	  })
-	  .catch((err) => console.log(err.message));
+        console.log(user);
+      });
+    })
+    .catch((err) => console.log(err.message));
 }
 
 runTests();
